80864d7e840f457f599529421cc66cb34caffa9f
Tomás <tomasgalli567@gmail.com>
2021-01-29 11:19:49 -0300

    Deploy3

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(express.json({ extended: true }))
 
 // puerto de la app
-const port = process.env.PORT || 4000;      // si tiene valor, si  no 4000
+const port = process.env.port || 4000;      // si tiene valor, si  no 4000
 
 // importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'))
@@ -31,6 +31,6 @@ app.use('/api/proyectos', require('./routes/proyectos'))
 app.use('/api/tareas', require('./routes/tareas'))
 
 // arrancar la app
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
     console.log(`El servidor está funcionando en el puerto ${port}`);
 });
