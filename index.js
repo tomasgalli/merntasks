a6c6266fa7d8c46fc8b9c5635ca7e1f0f7fce8b7
Tomás <tomasgalli567@gmail.com>
2021-01-29 10:52:17 -0300

    ajustes para deployment2

--- index.js
+++ index.js
@@ -32,5 +32,5 @@ app.use('/api/tareas', require('./routes/tareas'))
 
 // arrancar la app
 app.listen(port, '0.0.0.0', () => {
-    console.log(`El servidor está funcionando en el puerto ${PORT}`);
+    console.log(`El servidor está funcionando en el puerto ${port}`);
 });
